cde77eb93ddcb12848689ef6bb49b34b8d370a52
Paulo Pires <taralloqueiroiz@gmail.com>
2014-05-08 23:02:17 -0300

    imports

--- instagram/instagram.go
+++ instagram/instagram.go
@@ -1,5 +1,13 @@
 package instagram
 
+import "net/http"
+import "log"
+import "encoding/json"
+import "fmt"
+import "io/ioutil"
+import "os"
+import "github.com/gorilla/mux"
+
 type Pic struct {
 	Name    string `bson:"name"`
 	Picture string `bson:"picture"`
